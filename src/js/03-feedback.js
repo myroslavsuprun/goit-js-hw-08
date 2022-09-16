f84952603a8f480eb9e730ccbe8cf4af29a4e680
MiroProger <sirmiroslavsuprun@gmail.com>
2022-09-16 22:46:22 +0300

    Code refactoring, build correction

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,42 +10,33 @@ const LOCAL_FIELDS = 'feedback-form-state';
 ref.feedbackForm.addEventListener('input', throttle(handleDataInput, 500));
 ref.feedbackForm.addEventListener('submit', handleButtonSubmit);
 
-const storageValue = {
-  email: ref.emailForm.value,
-  message: ref.messageForm.value,
-};
-
 function handleButtonSubmit(e) {
   e.preventDefault();
-  const formEmail = ref.emailForm.value,
-    formMessage = ref.messageForm.value;
+
   console.log({
-    email: formEmail,
-    message: formMessage,
+    email: ref.emailForm.value,
+    message: ref.messageForm.value,
   });
+
   ref.emailForm.value = '';
   ref.messageForm.value = '';
   localStorage.removeItem(LOCAL_FIELDS);
 }
 
-function handleDataInput(e) {
-  if (e.target.name === 'email') {
-    storageValue.email = e.target.value;
-  } else {
-    storageValue.message = e.target.value;
-  }
-
-  localStorage.setItem(LOCAL_FIELDS, JSON.stringify(storageValue));
+function handleDataInput() {
+  const toAddValue = `{"${ref.emailForm.name}":"${ref.emailForm.value}", "${ref.messageForm.name}":"${ref.messageForm.value}"}`;
+  localStorage.setItem(LOCAL_FIELDS, toAddValue);
 }
 
-function clearWebStorageIfEmpty() {
-  if (localStorage.getItem(LOCAL_FIELDS)) {
-    const StorageFeedback = localStorage.getItem(LOCAL_FIELDS);
-    const StorageFeedbackParsed = JSON.parse(StorageFeedback);
+const storageValue = JSON.parse(localStorage.getItem(LOCAL_FIELDS));
 
-    ref.emailForm.value = StorageFeedbackParsed.email;
-    ref.messageForm.value = StorageFeedbackParsed.message;
-  }
+function addValuesIfNotEmpty() {
+  try {
+    if (storageValue.email || storageValue.message) {
+      ref.emailForm.value = storageValue.email;
+      ref.messageForm.value = storageValue.message;
+    }
+  } catch (error) {}
 }
 
-clearWebStorageIfEmpty();
+addValuesIfNotEmpty();
